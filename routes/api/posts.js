ffc83ca87de5c07fe1a291f25b51589f2a322b8a
thejuniordevops <edigithub@gmail.com>
2018-09-14 21:50:55 -0400

    initial commit

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router(); 
 
-//@route GET api/profile/test
-//@desc Tests profile route 
+
+//@route GET api/posts/test
+//@desc Tests post route 
 //@access Public 
 router.get('/test', (req, res) => {
 
-    res.json({msg:"Profile work"}); 
+    res.json({msg:"Posts work"}); 
 
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express(); 
 
 //DB Config 
@@ -25,6 +29,11 @@ app.get('/', (req, res) => {
     res.send('Hello');
 
 }); 
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const port = process.env.PORT || 5000; 
 
